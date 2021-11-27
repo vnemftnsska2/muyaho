d7f58a06f14c413d589b9b6c04f25152bf7edcac
tnsska2 <vnemftnsska2@naver.com>
2021-11-27 20:07:28 +0900

    Login Page

---
+++ src/components/login/login.jsx
@@ -0,0 +1,47 @@
+import React, { useRef } from 'react';
+import styles from './login.module.css';
+
+const Login = (props) => {
+    const formRef = useRef();
+    const idRef = useRef();
+    const passwordRef = useRef();
+
+    const onLoginSubmit = event => {
+        const loginInfo = {
+            id: idRef.current.value,
+            password: passwordRef.current.value,
+        };
+        
+        if (!loginInfo.id || !loginInfo.password) {
+            return alert('로그인 정보를 확인해주세요 :)');
+        }
+    };
+
+    return (
+        <div className={styles.login}>
+            <h1 className={styles.title}>무야호</h1>
+            <form 
+                className={styles.form}
+                ref={formRef}
+            >
+                <input
+                    type="text"
+                    placeholder="아이디를 입력해주세요"
+                    ref={idRef}
+                />
+                <input
+                    type="password"
+                    placeholder="비밀번호를 입력해주세요"
+                    ref={passwordRef}
+                />
+                <button
+                    type="button"
+                    className={styles.button}
+                    onClick={onLoginSubmit}
+                >LOGIN</button>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
