d528a235b14e3eac6ce479f0a8e8f7a0c7002ec4
tnsska2 <vnemftnsska2@naver.com>
2021-11-29 22:42:03 +0900

    Test Get Reading List

--- src/components/reading/reading.jsx
+++ src/components/reading/reading.jsx
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect, } from 'react';
 
 const Reading = () => {
+    const [readingList, setReadingList] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/reading', {
+            mode: 'no-cors',
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+        .then(res => console.log(res));
+    }, [readingList]);
+
     return (
         <h1>READING</h1>
     );
